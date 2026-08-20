5a3a7cb5f597e06658ae2b2baa890077d8048a8e
cleonavila <cleon@almagrario.com>
2026-08-19 21:42:46 -0500

    Se desarrolla componente reutilizable para manejo de errores

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,7 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { NgClass } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-
 import { environment } from '@env/environment';
 import { AuthService } from '@core/services/auth.service';
 import { Router } from '@angular/router';
@@ -11,7 +9,7 @@ import { FormError } from '@shared/components/form-error/form-error';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, InvalidClass, FormError, NgClass],
+  imports: [ReactiveFormsModule, InvalidClass, FormError],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
@@ -39,26 +37,13 @@ export default class Login {
     password: ['Lorena123', [Validators.required]],
   });
 
-  isFieldInvalid(fieldName: string): boolean {
-    const field = this.loginForm.get(fieldName);
-
-    return !!(
-      field &&
-      field.invalid &&
-      (field.dirty || field.touched)
-    );
-  }
-
   onSubmit(): void {
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
       return;
     }
-
     this.isLoading.set(true);
-
     const credentials = this.loginForm.getRawValue();
-
     this.authService.login(credentials).subscribe({
       next: response => {
         this.isLoading.set(false);
