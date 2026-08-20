fcfbae1e93e4540933c7dd84b6e2397ad11bded1
cleonavila <cleon@almagrario.com>
2026-08-19 21:59:14 -0500

    Se desarrolla componente reutilizable para manejo de errores

--- src/app/features/todo/pages/create-todo/create-todo.ts
+++ src/app/features/todo/pages/create-todo/create-todo.ts
@@ -1,11 +1,13 @@
 import { NgClass } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';;
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';import { FormError } from '@shared/components/form-error/form-error';
+import { InvalidClass } from '@shared/directives/invalid-class';
+;
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-create-todo',
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, InvalidClass, FormError],
   templateUrl: './create-todo.html',
   styleUrl: './create-todo.scss',
 })
@@ -14,6 +16,15 @@ export class CreateTodo {
   public isLoading = signal(false);
   private readonly formBuilder = inject(FormBuilder);
   private readonly toastr = inject(ToastrService);
+  errorsModel = {
+    title: {
+      required: "Debe digitar el título de la tarea",
+      minlength: "El título debe tener minimo 3 caracteres"
+    },
+    description: {
+      required: "Debe digitar la descripción de la tarea"
+    }
+  };
 
   public todoForm = this.formBuilder.group({
     'title': ['', [Validators.required, Validators.minLength(4)]],
@@ -23,18 +34,6 @@ export class CreateTodo {
   onSubmit() {
     if ( this.todoForm.invalid ) {
       this.todoForm.markAllAsTouched();
-      console.log(this.todoForm.get(''));
-
-      return;
     }
-  }
-
-  isFieldInvalid(fieldName: string): boolean {
-    const field = this.todoForm.get(fieldName);
-    return !!(
-      field &&
-      field.invalid &&
-      (field.dirty || field.touched)
-    );
   }
 }
